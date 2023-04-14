ffa2a9651123a0f2028be8652f915b1df5863ebe
kdpkst <jammiechen0@gmail.com>
2023-04-14 19:07:28 +0800

    add team list template

--- src/main/java/com/cpt202/team/Controllers/TeamController.java
+++ src/main/java/com/cpt202/team/Controllers/TeamController.java
@@ -3,6 +3,8 @@ package com.cpt202.team.Controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,7 +16,7 @@ import com.cpt202.team.Services.TeamService;
 
 
 // Spring Annotation
-@RestController
+@Controller
 @RequestMapping("/team")
 public class TeamController {
 
@@ -22,13 +24,14 @@ public class TeamController {
     private TeamService teamService;
 
     @GetMapping("/list")    
-    public List<Team> getList(){
-        return teamService.getTeamList();
+    public String getList(Model model){
+        model.addAttribute("teamList", teamService.getTeamList());
+        return "allTeams";
     }
 
-    @PostMapping("/add")
-    public Team addTeam(@RequestBody Team t){
-        return teamService.addTeam(t);
+    @GetMapping("/add")
+    public String addTeam(Model model){
+        return "addTeam"; 
     }
 
     
